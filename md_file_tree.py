60680983ca105c9c24e33e28838edea74abee08b
sunxvming <466205048@qq.com>
2021-04-12 17:46:17 +0800

    update blog

--- md_file_tree.py
+++ md_file_tree.py
@@ -113,17 +113,22 @@ def create_index(cwd, headings=False, wikilinks=False):
                                                             TOC_LIST_PREFIX))
             rel_dir = '.{1}{0}'.format(os.sep, root[base_len:])
             for md_filename in files:
+                md_filename_without_ext = os.path.splitext(md_filename)[0]
+                # 链接中的空格替换掉，不然markdown解析不了链接 比如：[Google C++ Style Guide](./Google C++ Style Guide)
+                md_filename = md_filename.replace(" ", "&#32;")
                 indent = '  ' * level
                 if wikilinks:
-                    md_lines.append('{0} {3} [[{2}{1}]]\n'.format(indent,
-                                                                  os.path.splitext(md_filename)[0],
+                    md_lines.append('{0} {3} [[{2}{4}]]\n'.format(indent,
+                                                                  md_filename_without_ext,
                                                                   rel_dir,
-                                                                  TOC_LIST_PREFIX))
+                                                                  TOC_LIST_PREFIX,
+                                                                  md_filename))
                 else:
-                    md_lines.append('{0} {3} [{1}]({2}{1})\n'.format(indent,
-                                                                     md_filename,
+                    md_lines.append('{0} {3} [{1}]({2}{4})\n'.format(indent,
+                                                                     md_filename_without_ext,
                                                                      rel_dir,
-                                                                     TOC_LIST_PREFIX))
+                                                                     TOC_LIST_PREFIX,
+                                                                     md_filename))
                 if headings:
                     results = get_headers(os.path.join(root, md_filename))
                     if len(results) > 0:
